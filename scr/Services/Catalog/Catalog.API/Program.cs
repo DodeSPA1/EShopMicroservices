0460b43e46d4351db0b1053d9d2badf9e1a28aae
Jorge Serrada <jorgeserrada1@gmail.com>
2025-09-24 08:41:40 +0200

    wip: Develop Create Product Endpoint with Minimal Apis and Carter

--- scr/Services/Catalog/Catalog.API/Program.cs
+++ scr/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//builder.Services.AddCarter();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+//app.MapCarter();
 app.Run();
