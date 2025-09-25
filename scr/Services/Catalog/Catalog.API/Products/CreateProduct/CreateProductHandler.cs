1676e648b8ad25e8ed9b11d423b57725c4e7ba9f
Jorge Serrada <jorgeserrada1@gmail.com>
2025-09-25 08:06:51 +0200

    wip: Develop CommandHandler to Save Product to DB using Marten Library

--- scr/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ scr/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
 
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler (IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -23,9 +19,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price
             };
             // Save entity to database.
-
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
             // return result with new product id.(CreateProductResult)
-             return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
