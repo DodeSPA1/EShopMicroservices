8a49cba818b4ff31ddbda88c93c3ec36269fd65a
Jorge Serrada <jorgeserrada1@gmail.com>
2025-09-26 11:57:55 +0200

    wip:Develop GetProducts GET Endpoint with Minimal Apis and Carter

--- scr/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ scr/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,22 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProiductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint:ICarterModule
 {
-    public class GetProductsEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProiductsResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProiductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
     }
 }
+
