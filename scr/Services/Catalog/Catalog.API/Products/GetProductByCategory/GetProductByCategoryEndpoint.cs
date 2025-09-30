4f1a054cc5a8a9258ef28f05d49c5d4136a4732a
Jorge Serrada <jorgeserrada1@gmail.com>
2025-09-30 09:01:34 +0200

    feat:  Develop GetProductByCategory GET Endpoint with Minimal Apis and Carter

--- scr/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ scr/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -1,8 +1,22 @@
 ﻿namespace Catalog.API.Products.GetProductByCategory
 {
-
+    public record  GetProductByCategoryQueryResponse(IEnumerable<Product> Products)  ;
     public class GetProductByCategoryEndpoint
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+                var response = result.Adapt<GetProductByCategoryQueryResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductByCategoryQueryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products By Category")
+            .WithDescription("Get Products By Category");
+        }
 
     }
 }
