51dce8049e6e7fb2f48d534d00e14ea8de5be89e
Jorge Serrada <jorgeserrada1@gmail.com>
2025-09-30 07:54:32 +0200

    wip: Develop GetProductById Handler with CQRS and MediatR

---
+++ scr/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException()
+        {
+        }
+
+        public ProductNotFoundException() : base("Product not found!")
+        {
+        }
+    }
+}
---
+++ scr/Services/Catalog/Catalog.API/Products/GetProductById/GetProductsByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductsById;
+
+public record GetProiductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsByIdEndpoint:ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProiductsResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProiductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
+
