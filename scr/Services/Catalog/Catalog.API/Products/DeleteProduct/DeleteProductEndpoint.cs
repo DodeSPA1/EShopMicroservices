554c70f88bae6cb8aa392ba99985932b2e4497e3
Jorge Serrada <jorgeserrada1@gmail.com>
2025-10-01 20:55:09 +0200

    feat:  Develop DeleteProduct DELETE Endpoint with Minimal Apis and Carter

--- scr/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ scr/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,6 +1,25 @@
-﻿namespace Catalog.API.Products.DeleteProducts
+﻿using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProducts
 {
+    public record DeProductRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
     public class DeleteProductEndpoint
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid Id, ISender sender) =>
+            {
+         
+                var result = await sender.Send(new DeleteProductCommand(Id));
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("Delete Product")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Delete Product")
+                .WithDescription("Delete Product");
+        }
     }
 }
