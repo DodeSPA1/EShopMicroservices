aaaccdd38951678276f4a2a4817423f4048364c7
Jorge Serrada <jorgeserrada1@gmail.com>
2025-10-09 07:51:56 +0200

    feat: BuildingBlocks Handling Exceptions Generic Way w/ app.UseExceptionHandler

--- scr/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ scr/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,11 +1,12 @@
-﻿using System.Runtime.Serialization;
+﻿using BuildingBlocks.Exceptions;
+using System.Runtime.Serialization;
 
 namespace Catalog.API.Exceptions
 {
-    public class ProductNotFoundException : Exception
+    public class ProductNotFoundException : NotFoundException
     {
 
-        public ProductNotFoundException() : base("Product not found!")
+        public ProductNotFoundException(Guid Id) : base("Product", Id)
         {
         }
     }
